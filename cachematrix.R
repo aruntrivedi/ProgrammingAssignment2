661fbabbc3cbc22c6fc120250b11fe470e0d2a47
aruntrivedi <trivediarun3@gmail.com>
2015-08-21 16:21:32 +0530

    my first commit on cacheSolve (matrix inverse)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Open R Studio and setwd() to the directory eg.c:/users/username/cache1/ProgrammingAssignment2/cachematrix.R
+## "cache1" is the local repo where forked file is cloned
+## Open cahematrix.R file forked from github repo to define the function
+
+## create makeCacheMatrix () function to define set of functions 
+## these functions are to set and get the value of the matrix
+## it also has a function call to CashSolve which will inverse value of matrix defined in makeCacheMatrix()
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m <- NULL
+setmatrix <- function (y) {
+  x <<- y
+  m <- NULL
+}
+getmatrix <- function () x
+cacheinv <- function (inv) m <<- inv
+getinv <- function () m
+list (setmatrix=setmatrix, getmatrix=getmatrix, cacheinv=cacheinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## function expects input matrix to be inverted
+## input matrix is assigned to m
+## retrieve cache if its not NULL 
+## else matrix is populated using getmatrix and
+## assigned to z
+## matrixinverse function is executed on z and
+## set inverted matrix is done on m which is final
+## value of the inverted matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv ()
+  if (!is.null(m)){
+    message("getting cached matrix")
+    return(m)
+    }
+  z <- x$getmatrix()
+  m <- solve(z) %*% z
+  x$cacheinv(m)
+  ## Return a matrix that is the inverse of 'x'
+  m
 }
